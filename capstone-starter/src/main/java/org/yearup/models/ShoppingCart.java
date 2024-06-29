b0c95809b31038997dff2979cc465fde85fc2f11
eric carpo <carpoeric@gmail.com>
2024-06-29 00:30:22 -0700

    cleaning code

--- capstone-starter/src/main/java/org/yearup/models/ShoppingCart.java
+++ capstone-starter/src/main/java/org/yearup/models/ShoppingCart.java
@@ -1,13 +1,19 @@
 package org.yearup.models;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ShoppingCart
 {
     private Map<Integer, ShoppingCartItem> items = new HashMap<>();
 
+
+    public List<ShoppingCartItem> returnAsList() {
+        return new ArrayList<>(items.values());
+    }
     public Map<Integer, ShoppingCartItem> getItems()
     {
         return items;
@@ -17,6 +23,7 @@ public class ShoppingCart
     {
         this.items = items;
     }
+
 
     public boolean contains(int productId)
     {
@@ -36,9 +43,9 @@ public class ShoppingCart
     public BigDecimal getTotal()
     {
         BigDecimal total = items.values()
-                                .stream()
-                                .map(i -> i.getLineTotal())
-                                .reduce( BigDecimal.ZERO, (lineTotal, subTotal) -> subTotal.add(lineTotal));
+                .stream()
+                .map(i -> i.getLineTotal())
+                .reduce( BigDecimal.ZERO, (lineTotal, subTotal) -> subTotal.add(lineTotal));
 
         return total;
     }
